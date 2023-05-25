c359ee9163ec29e51d54b112446ade9f485941d3
enee-src <134647692+enee-src@users.noreply.github.com>
2023-05-25 14:15:04 -0600

    Add files via upload

---
+++ layers/CLIENTES_22.js
@@ -0,0 +1,8 @@
+var json_CLIENTES_22 = {
+"type": "FeatureCollection",
+"name": "CLIENTES_22",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "COSIGOCLIENTESGD": "1", "CLAVE": 2008801.0, "ID_TRANSFORMADOR": 1041684.0, "COORD_X": 269851.3, "COORD_Y": 1601271.23, "CIRCUITO": "SRS339", "CODIGOCLIENTESGD": 3469828.0, "DEPARTAMENTO": "OCOTEPEQUE", "MUNICIPIO": "SINUAPA", "ALDEA": "LA LABORCITA", "COLONIA": "LA LABORCITA" }, "geometry": { "type": "Point", "coordinates": [ -89.135207198893056, 14.474448478901358 ] } },
+]
+}
